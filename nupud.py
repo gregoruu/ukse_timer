cc1208dfa6094a2f554b750548063e0bbfc2bc51
Gregor Uustalu <gregor.uustalu@gmail.com>
2025-02-06 20:43:39 +0200

    Update nupud.py

--- nupud.py
+++ nupud.py
@@ -3,3 +3,4 @@ from evdev import InputDevice, list_devices
 devices = [InputDevice(path) for path in list_devices()]
 for device in devices:
     print(device.path, device.name, device.phys)
+    # RUN+="/bin/sh -c 'echo enabled > /sys/bus/usb/devices/$kernel/power/wakeup'"
